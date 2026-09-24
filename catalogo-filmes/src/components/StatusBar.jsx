409195cad54c65f015ebaf7277be92726801cfbe
Rebeca de Oliveira Scutare <rebecascutare@gmail.com>
2026-09-24 10:48:07 -0300

    Delete catalogo-filmes directory

--- catalogo-filmes/src/components/StatusBar.jsx
+++
@@ -1,9 +0,0 @@
-function StatusBar({ message }) {
-  return (
-    <header className="statusbar">
-      <span className="statusbar__message">{message}</span>
-    </header>
-  );
-}
-
-export default StatusBar;
